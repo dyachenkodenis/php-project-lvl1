36424982354edd3138f115c819d675857bd77ac1
dyachenkodenis <denisdyachenko.90@gmail.com>
2021-05-04 12:10:42 +0500

    добавил клас Cli.php

---
+++ src/Cli.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Brain\Games\Cli;
+
+require_once '../vendor/autoload.php';
+
+use function cli\line;
+use function cli\prompt;
+
+
+function run()
+{
+  line("Welcome to the Brain Games!");
+  $name = prompt('May I have your name?');
+  line("Hello, %s!", $name);
+}
+
+echo run();
