0cfe2e404c7e5399f0bbc93b946666a986ddb7af
dyachenkodenis <denisdyachenko.90@gmail.com>
2021-05-18 19:03:44 +0500

    внес правки на ошибки линтера

--- src/Cli.php
+++ src/Cli.php
@@ -2,17 +2,9 @@
 
 namespace Brain\Games\Cli;
 
-require_once '../vendor/autoload.php';
-
-use function cli\line;
-use function cli\prompt;
-
-
 function run()
 {
     line("Welcome to the Brain Games!");
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
 }
-
-echo run();
